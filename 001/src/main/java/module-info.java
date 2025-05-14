06b329270565b5f8ce3c7e3cc0e47e900a39a820
Raza <141234596+KMORaza@users.noreply.github.com>
2025-05-14 17:36:35 +0530

    Delete 001/src/main directory

--- 001/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.spectrumanalyzer {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens spectrum.analyzer.software to javafx.fxml;
-    exports spectrum.analyzer.software;
-}
